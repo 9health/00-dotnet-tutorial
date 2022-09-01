430d9b474164179ed7289dcd9922e465a47f4cab
9Health <ninehealth at outlook dot com>
2022-09-02 04:38:54 +0700

    Add small program to calculate sum from 1 to 10

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -5,3 +5,13 @@ Console.WriteLine("Hello, World!");
 
 Console.WriteLine("The current time is " + DateTime.Now);
 
+int s;
+
+s = 0;
+
+for ( int i = 1; i <= 10; i++ ) {
+  s = s + i;
+}
+
+Console.WriteLine("Sum from 1 to 10 is " + s);
+
