ad4097a6616fec4cecab8f730d2daf33dd379390
9Health <ninehealth at outlook dot com>
2022-09-02 15:33:07 +0700

    Add Hello World from old template

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,17 +1,27 @@
 ﻿
 // See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System;
 
-Console.WriteLine("The current time is " + DateTime.Now);
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
 
-int s;
+            int s;
 
-s = 0;
+            s = 0;
 
-for ( int i = 1; i <= 10; i++ ) {
-  s = s + i;
+            for ( int i = 1; i <= 10; i++ ) {
+              s = s + i;
+            }
+
+            Console.WriteLine("Sum from 1 to 10 is " + s);
+        }
+    }
 }
-
-Console.WriteLine("Sum from 1 to 10 is " + s);
 
