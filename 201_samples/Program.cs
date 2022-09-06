25c9b5a64e066efc804e5ff3336a00ec3dfc6975
9health <ninehealth@outlook.com>
2022-09-06 18:46:23 +0700

    Add nullable test examples 01, 02

---
+++ 201_samples/Program.cs
@@ -0,0 +1,42 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// 201 Samples at here
+
+_201_samples._01_test_nullable();
+
+public class _201_samples {
+
+    // Test nullable type int?
+    public static void _01_test_nullable() {
+
+        int  a;
+        int? ax;
+
+        a = 4;
+        Console.WriteLine($"a = {a}. Expect a = 4");
+
+        ax = null;
+        Console.WriteLine($"ax = {ax}. Expect ax = (null)");
+
+    }
+
+    // Try exception if null is assigned to int
+    // New idea is using random to trick the compiler!!!
+    public static void _02_test_nullable_exception() {
+
+        int  a;
+
+        // 2022/Sep/06: Build failed.
+        // try {
+        //    a = null;
+        // }
+        // catch (Exception e) {
+        //     Console.WriteLine("Exception happened.");
+        // }
+
+    }
+
+}
+
