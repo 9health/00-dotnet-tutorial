e82b4bd1aba023c7f1c381d376a285b83056f039
9health <ninehealth@outlook.com>
2022-09-07 17:29:33 +0700

    Add exception check for nullable type

--- 201_samples/Program.cs
+++ 201_samples/Program.cs
@@ -4,7 +4,8 @@ Console.WriteLine("Hello, World!");
 
 // 201 Samples at here
 
-_201_samples._01_test_nullable();
+// _201_samples._01_test_nullable();
+_201_samples._02_test_nullable_exception();
 
 public class _201_samples {
 
@@ -26,15 +27,28 @@ public class _201_samples {
     // New idea is using random to trick the compiler!!!
     public static void _02_test_nullable_exception() {
 
-        int  a;
+        var  rand = new Random();
+        var  randDouble = rand.NextDouble();
 
-        // 2022/Sep/06: Build failed.
-        // try {
-        //    a = null;
-        // }
-        // catch (Exception e) {
-        //     Console.WriteLine("Exception happened.");
-        // }
+        int? ingredient_id_db;
+        int  ingredient_id_get;
+
+
+        for ( int i=0; i<4; ++i ) {
+            randDouble = rand.NextDouble();
+            ingredient_id_db = ( randDouble >= 0.5) ? null : (int)(randDouble*100);
+
+            // 2022/Sep/06: Build failed.
+            // 2022/Sep/07: Exception generated at runtime!
+            try {
+               ingredient_id_get = (int)ingredient_id_db;
+               Console.WriteLine($"ingredient_id_get = {ingredient_id_get}. Expect ingredient_id_get = ");
+            }
+            catch (Exception e) {
+                Console.WriteLine($"Exception happened. e.GetType() = {e.GetType()}");
+            }
+        }
+
 
     }
 
