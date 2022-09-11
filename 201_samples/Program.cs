32f2c224fb9cc349e48fb41219424c01d0a30ee7
9health <ninehealth@outlook.com>
2022-09-11 10:54:31 +0700

    Add implicitly typed local variables example

--- 201_samples/Program.cs
+++ 201_samples/Program.cs
@@ -5,5 +5,8 @@ Console.WriteLine("Hello, World!");
 // 201 Samples at here
 
 // _201_samples._01_test_nullable();
-_201_samples._02_test_nullable_exception();
+// _201_samples._02_test_nullable_exception();
+
+// _201_samples._10_test_array();
+_201_samples._11_test_var();
 
--- 201_samples/libs/_201_samples.cs
+++ 201_samples/libs/_201_samples.cs
@@ -41,7 +41,62 @@ public class _201_samples {
             }
         }
 
+    }
 
+    // Try arrays
+    public static void _10_test_array() {
+        int[] food_time_A;
+
+        food_time_A = new int[] {10, 20, 30, 40};
+
+        _201_libs.PrintValues(food_time_A);
+
+        int[] food_time_B = new int[] {10, 20, 30, 40, 50};
+        food_time_B[0] = 10;
+        food_time_B[1] = 10;
+        food_time_B[2] = 10;
+        food_time_B[3] = 10;
+        food_time_B[4] = 10;
+
+        _201_libs.PrintValues(food_time_B);
+
+        int[] food_time_C = new int[7] {10, 20, 30, 40, 50, 60, 70};
+
+        _201_libs.PrintValues(food_time_C);
+
+        int[] food_time_D = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+
+        _201_libs.PrintValues(food_time_D);
+    }
+
+    // Try vars
+    public static void _11_test_var() {
+
+        var food_name_1 = "Thit bo xao rau cai";
+
+        Console.WriteLine($"food_name_1 = {food_name_1}");
+        Console.WriteLine($"food_name_1.GetType() = {food_name_1.GetType()}");
+
+        var ingredient_num = 5;
+        Console.WriteLine($"ingredient_num = {ingredient_num}");
+        Console.WriteLine($"ingredient_num.GetType() = {ingredient_num.GetType()}");
+
+        // var[] food1_steps = {"1. Rua rau", "2. Thai hanh", "3. Xao"};
+        var food1_steps = new[] {"1. Rua rau", "2. Thai hanh", "3. Xao"};
+        _201_libs.PrintValues(food1_steps);
+        Console.WriteLine($"food1_steps.GetType() = {food1_steps.GetType()}");
+
+        var food1 = new { ID = 1, Name = "Thit bo xao rau cai", Steps = 4  };
+        Console.WriteLine($"food1 = {food1}");
+        Console.WriteLine($"food1.GetType() = {food1.GetType()}");
+
+        var food2 = new { ID = 2, Name = "Thit bo xao rau cai", Steps = 4  };
+        Console.WriteLine($"food2 = {food2}");
+        Console.WriteLine($"food2.GetType() = {food2.GetType()}");
+
+        var food_list = new {food1, food2};
+        Console.WriteLine($"food_list = {food_list}");
+        Console.WriteLine($"food_list.GetType() = {food_list.GetType()}");
     }
 
 }
