68ece792aaf7779a53e4de11c547a076474bab46
Viet Quang Vu <vuvietquang90@gmail.com>
2022-09-12 09:34:18 +0700

    Working with data using entity framework core

---
+++ GlobalNamespace/GlobalUsings.cs
@@ -0,0 +1,17 @@
+﻿global using System;
+global using System.Linq;
+global using System.Collections.Generic;
+global using static System.Console;
+global using static System.Environment;
+global using static System.IO.Path;
+global using System.Xml;
+global using System.Text.Json; // JsonSerializer
+global using System.Text.Json.Serialization; // [JsonInclude]
+global using Microsoft.EntityFrameworkCore; // DbContext, DbContextOptionsBuilder
+global using System.ComponentModel.DataAnnotations.Schema; // [Column]
+global using System.ComponentModel.DataAnnotations; // [Required], [StringLength]
+// for consoleLogger
+global using Microsoft.EntityFrameworkCore.Infrastructure;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
+//Courses and students
---
+++ WorkingWithEFCore/Northwind.cs
@@ -0,0 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage; // IDbContextTransaction
+
+namespace Packt.Shared;
+// this manages the connection to the database
+public class Northwind: DbContext
+{
+    // these properties map to tables in the database
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<Product>? Products { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseLazyLoadingProxies(); //enable lazy loading
+
+        if (ProjectConstants.DatabaseProvider == "SQLite")
+        {
+            string path = Combine(CurrentDirectory, "Northwind.db");
+            WriteLine($"Using {path} database file.");
+            optionsBuilder.UseSqlServer($"Filename={path}");
+        }
+        else
+        {
+            string connection = "Data Source=tcp:192.168.1.5,1433;" +
+                        "Initial Catalog=Northwind;" +
+                        "User Id=sa;" +
+                        "Password=1Lik39Health!;" +
+                        "Integrated Security = false;" +
+                        "MultipleActiveResultSets = true;";
+            optionsBuilder.UseSqlServer(connection);
+        }
+    }
+    protected override void OnModelCreating(
+      ModelBuilder modelBuilder)
+    {
+        // example of using Fluent API instead of attributes
+        // to limit the length of a category name to 15
+        modelBuilder.Entity<Category>()
+          .Property(category => category.CategoryName)
+          .IsRequired() // NOT NULL
+          .HasMaxLength(15);
+        if (ProjectConstants.DatabaseProvider == "SQLite")
+        {
+            // added to "fix" the lack of decimal support in SQLite
+            modelBuilder.Entity<Product>()
+              .Property(product => product.Cost)
+              .HasConversion<double>();
+        }
+        modelBuilder.Entity<Product>()
+            .HasQueryFilter(p => !p.Discontinued);
+    }
+}
