60105939c2632e9130d33c5e889e48b8892fcfd9
quangvv9Life <105659664+quangvv9Life@users.noreply.github.com>
2022-09-12 09:37:24 +0700

    Update Northwind.cs

--- WorkingWithEFCore/AutoGenModels/Northwind.cs
+++ WorkingWithEFCore/AutoGenModels/Northwind.cs
@@ -24,7 +24,7 @@ namespace WorkingWithEFCore.AutoGen
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=tcp:192.168.1.5,1433;Initial Catalog=Northwind;Persist Security Info=True;User ID=SA;Password=1Lik39Health!");
+                optionsBuilder.UseSqlServer("Data Source=tcp:*********,1433;Initial Catalog=Northwind;Persist Security Info=True;User ID=SA;Password=*********");
             }
         }
 
