942b59d16288c8d848eb455ea9c7b1cb76da7b88
Viet Quang Vu <vuvietquang90@gmail.com>
2022-09-14 22:09:23 +0700

    improving performance and scalability using multitasking

---
+++ GlobalNamespace/GlobalUsings.cs
@@ -0,0 +1,22 @@
+﻿global using System;
+//global using System.Linq;
+//global using System.Collections.Generic;
+global using static System.Console;
+//global using static System.Environment;
+//global using static System.IO.Path;
+//global using System.Xml;
+//global using System.Text.Json; // JsonSerializer
+//global using System.Text.Json.Serialization; // [JsonInclude]
+//global using Microsoft.EntityFrameworkCore; // // DbContext, DbSet<T>, DbContextOptionsBuilder
+//global using System.ComponentModel.DataAnnotations.Schema; // [Column]
+//global using System.ComponentModel.DataAnnotations; // [Required], [StringLength]
+// for consoleLogger
+//global using Microsoft.EntityFrameworkCore.Infrastructure;
+//global using Microsoft.Extensions.DependencyInjection;
+//global using Microsoft.Extensions.Logging;    
+//LinQ in Parallel
+global using System.Diagnostics;
+global using static System.Diagnostics.Process;
+// benchmarking
+global using BenchmarkDotNet.Running;
+global using BenchmarkDotNet.Attributes; // [Benchmark]
