5dd2a1d6dc186fd27ba6b402f4d20334f2eb3cdd
9Health <ninehealth@outlook.com>
2022-09-21 10:57:27 +0000

    Add simple food DB

---
+++ 201_samples/libs/NHFoodVerF.cs
@@ -0,0 +1,15 @@
+
+public class NHFoodVerE {
+
+    // Auto-implemented properties
+    public  int  FoodId          { get; set; }
+    public  int  FoodTime        { get; set; }
+    public  int  FoodSteps       { get; set; }
+
+    public  int  FoodViews       { get; set; }
+    public  int  IngredientNum   { get; set; }
+
+    public  string FoodName      { get; set; }
+
+}
+
---
+++ 201_samples/models/FoodModel.cs
@@ -0,0 +1,24 @@
+
+using Microsoft.EntityFramework.Core;
+using System;
+using System.Collection.Generic;
+
+public class FoodModel : DbContext {
+
+    public  DbSet<NHFoodVerF>  Foods { get; set; }
+
+    public  string  DbPath { get; }
+
+    public  FoodModel() {
+        var  folder  =  Environment.SpecialFolder.LocalApplicationData;
+        var  path    =  Environment.GetFolderPath(folder);
+        DbPath       =  System.IO.Path.Join(path, "food.db");
+    }
+
+    // The following configure EF to create a SQLite database file on
+    // special "local" folder.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+
+}
+
