79096e59926230bd36ad49aa1db0831b00c50870
9Health <ninehealth@outlook.com>
2022-09-21 12:42:58 +0000

    Add food and meal class version G

---
+++ 201_samples/libs/MealVerG.cs
@@ -0,0 +1,64 @@
+
+using System.Collections;
+
+// [NHFoodVerGAttribute("Hai", "v0.1", false)]
+public class MealVerG : IDisposable {
+
+    // Members
+    public List<NHFoodVerG> foods = new List<NHFoodVerG>();
+
+    // Public methods
+    public void AddFood(int foodID, string foodName, int foodTime) {
+        foods.Add(new NHFoodVerG { FoodId = foodID, FoodName = foodName, FoodTime = foodTime } );
+    }
+
+    public IEnumerator GetEnumerator() {
+        foreach( var food in foods ) {
+            yield return food.FoodName;
+        }
+    }
+
+    // Public members
+    public IEnumerable FoodTimeQuick {
+        get { return FoodTimeType(1, 5); }
+    }
+
+    public IEnumerable FoodTimeMedium {
+        get { return FoodTimeType(5, 15); }
+    }
+
+    public IEnumerable FoodTimeLong {
+        get { return FoodTimeType(15, 60); }
+    }
+
+ // public IEnumerable<NHFoodVerG> TopN(int n) {
+ //     for ( var index=0; index<n; index++ ) {
+ //         yield return foods[index];
+ //     }
+ // }
+
+    public IEnumerable TopN(int n) {
+        for ( var index=0; index<n; index++ ) {
+            yield return "[" + foods[index].FoodId + "] " + foods[index].FoodName;
+        }
+    }
+
+    public IEnumerable PageN(int n) {
+        var stopIndex  = n*3;
+        var startIndex = stopIndex-3;
+        for ( var index=startIndex; index<stopIndex; index++ ) {
+            yield return "[" + foods[index].FoodId + "] " + foods[index].FoodName;
+        }
+    }
+
+    // Private methods
+    private IEnumerable FoodTimeType(int minTime, int maxTime) {
+        foreach ( var food in foods ) {
+            if ( (food.FoodTime > minTime) && (food.FoodTime <= maxTime) ) {
+                yield return food.FoodName +  " (" + food.FoodTime + ")";
+            }
+        }
+    }
+
+}
+
---
+++ 201_samples/libs/NHFoodVerG.cs
@@ -0,0 +1,45 @@
+
+public class NHFoodVerG {
+
+    // Auto-implemented properties
+    public  int  FoodId          { get; set; }
+    public  int  FoodTime        { get; set; }
+    public  int  FoodSteps       { get; set; }
+
+    [NonSerialized()] int _FoodViewsPrv ;
+    [NonSerialized()] public  int  FoodViewsPbl ;                
+    // Compile error CS0592
+    // [NonSerialized()] public  int  FoodViewsPrvSet { get; private set; }
+
+    [NonSerialized()] public  int  FoodViewsPrvSet ;
+
+    public  int  FoodViews       { get; set; }
+    public  int  IngredientNum   { get; set; }
+
+    [NonSerialized()] public  int  FoodViewsPrvOK; 
+    // public  int  FoodViewsPrvOK  {
+    //     get =>  _FoodViewsPrv;
+    //     set =>  _FoodViewsPrv = value;
+    // }
+
+    public  string FoodName      { get; set; }
+
+    // Constructor
+    public NHFoodVerG() {
+        FoodId        = 0      ;
+        FoodTime      = 0      ;
+        FoodSteps     = 0      ;
+        FoodViews     = 0      ;
+        IngredientNum = 0      ;
+    }
+
+    public NHFoodVerG(int id, int time, int steps, int views, int inum) {
+        FoodId        = id     ;
+        FoodTime      = time   ;
+        FoodSteps     = steps  ;
+        FoodViews     = views  ;
+        IngredientNum = inum   ;
+    }
+
+}
+
