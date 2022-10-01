b1eddc008a8799cb05c7594f0110c8df24713ac7
Viet Quang Vu <vuvietquang90@gmail.com>
2022-10-01 22:13:28 +0700

    DP-open-and-close

---
+++ C03/Open Close/tryNinjaBeforeOCP/Program.cs
@@ -0,0 +1,16 @@
+using tryNinjaBeforeOCP;
+using tryNinjaShared;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", async (HttpContext context) =>
+{
+    //Create actors
+    var target = new Ninja("The Unseen Mirage", new Sword(), new Pistol());
+    var ninja = new Ninja("The Blue Phantom", new Sword(), new Shuriken());
+    // Execute the sequence of actions
+    await Logic.ExecuteSequenceAsync(ninja, target, writeAsync: s => context.Response.WriteAsync(s));
+});
+
+app.Run();
