b1d7ec364c589f242849289f8546a9194d3f246d
9Health <ninehealth@outlook.com>
2022-10-08 00:53:47 +0700

    Update handy SQL script

---
+++ 201_samples/sql/delete_meals.sql
@@ -0,0 +1,10 @@
+--
+-- HELP
+--    From cmd
+--    sqlite3 -init sql\delete_meal.sql db\food.sqlite .quit
+-- 
+-- List all meal
+DELETE FROM Meals;
+DELETE FROM sqlite_sequence WHERE NAME='Meals';
+SELECT * FROM sqlite_sequence;
+
